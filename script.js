8cf91c8e330ce7020c6f2c7360b4713eae58c41f
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2023-12-01 20:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	for(i=0;i<string.length;i++){
+		 let char = str[i];
+    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return null;
+
+	}
+	
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
